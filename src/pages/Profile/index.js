da74b69dbe5256522f3a30d2a28db91b22510f4a
Vitor Hugo Scobin <vhscobin@gmail.com>
2019-07-26 15:17:08 -0300

    Page profile saga and actions

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,40 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
-// import { Container } from './styles';
+import { Form, Input } from '@rocketseat/unform';
+import { Container } from './styles';
+
+import { updateProfileRequest } from '~/store/modules/user/actions';
 
 export default function Profile() {
-  return <h1>Profile</h1>;
+  const dispatch = useDispatch();
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {
+    dispatch(updateProfileRequest(data));
+  }
+
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Email" disabled />
+        <hr />
+        <Input
+          name="oldPassword"
+          type="password"
+          placeholder="Sua senha atual"
+        />
+
+        <Input name="newPassword" type="password" placeholder="Nova senha" />
+        <Input
+          name="confirmPassword"
+          type="password"
+          placeholder="Confirmar nova senha"
+        />
+        <button type="submit">Atualizar perfil</button>
+      </Form>
+      <button type="button">Sair da aplicação</button>
+    </Container>
+  );
 }
